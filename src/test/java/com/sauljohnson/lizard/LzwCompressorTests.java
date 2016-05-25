4a872d7043c50fd09dce1c5ae07bf8e6f8493a2d
Saul Johnson <saul.a.johnson@gmail.com>
2016-05-25 20:02:19 +0100

    Add source code

---
+++ src/test/java/com/sauljohnson/lizard/LzwCompressorTests.java
@@ -0,0 +1,13 @@
+package com.sauljohnson.lizard;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for the {@link LzwCompressor} class.
+ *
+ * @version 1.0 25 May 2016
+ * @author  Saul Johnson
+ */
+public class LzwCompressorTests extends TestCase {
+
+}
