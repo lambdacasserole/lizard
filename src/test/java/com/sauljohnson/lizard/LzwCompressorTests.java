97bfd4f36ed0503da158837af801b1c062b58edb
Saul Johnson <saul.a.johnson@gmail.com>
2016-05-26 00:19:01 +0100

    Add tests for LzwCompressor

--- src/test/java/com/sauljohnson/lizard/LzwCompressorTests.java
+++ src/test/java/com/sauljohnson/lizard/LzwCompressorTests.java
@@ -2,6 +2,8 @@ package com.sauljohnson.lizard;
 
 import junit.framework.TestCase;
 
+import java.io.IOException;
+
 /**
  * Unit tests for the {@link LzwCompressor} class.
  *
@@ -10,4 +12,38 @@ import junit.framework.TestCase;
  */
 public class LzwCompressorTests extends TestCase {
 
+    public void testCompress() {
+        // Create compressor and data to compress.
+        LzwCompressor subject = new LzwCompressor();
+        byte[] data = new byte[] {0, 16};
+
+        // Try to compress data.
+        try {
+            byte[] compressed = subject.compress(data);
+
+            // For a small array this will actually increase the size.
+            assertEquals(4, compressed.length);
+            assertEquals(16, compressed[3]);
+        } catch (IOException ex) {
+            fail("Exception encountered during compression.");
+        }
+    }
+
+    public void testDecompress() {
+        // Create compressor and data to decompress.
+        LzwCompressor subject = new LzwCompressor();
+        byte[] data = new byte[] {0, 0, 0, 16};
+
+        // Try to decompress data.
+        try {
+            byte[] decompressed = subject.decompress(data);
+
+            // This should actually decrease the size.
+            assertEquals(2, decompressed.length);
+            assertEquals(0, decompressed[0]);
+            assertEquals(16, decompressed[1]);
+        } catch (IOException ex) {
+            fail("Exception encountered during decompression.");
+        }
+    }
 }
