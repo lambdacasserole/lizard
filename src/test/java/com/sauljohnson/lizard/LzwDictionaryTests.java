2e84cb5cf29d9d1a02f77f989e10d91e1b5c122e
Saul Johnson <saul.a.johnson@gmail.com>
2016-05-26 15:38:04 +0100

    Unit test LzwDictionary

--- src/test/java/com/sauljohnson/lizard/LzwDictionaryTests.java
+++ src/test/java/com/sauljohnson/lizard/LzwDictionaryTests.java
@@ -10,4 +10,56 @@ import junit.framework.TestCase;
  */
 public class LzwDictionaryTests extends TestCase {
 
+    public void testHasData() {
+        // Initialize dictionary with one entry.
+        LzwDictionary subject = new LzwDictionary();
+        subject.addData("ff");
+
+        // Dictionary should have data for "ff".
+        assertTrue(subject.hasData("ff"));
+    }
+
+    public void testGetCode() {
+        // Initialize dictionary with one entry.
+        LzwDictionary subject = new LzwDictionary();
+        subject.addData("ff");
+
+        // Dictionary should contain code 0 against "ff".
+        assertEquals(0, (int) subject.getCode("ff"));
+    }
+
+    public void testGetData() {
+        // Initialize dictionary with one entry.
+        LzwDictionary subject = new LzwDictionary();
+        subject.addData("ff");
+
+        // Dictionary should return correct data for code 0.
+        assertEquals("ff", subject.getData((short) 0));
+    }
+
+    public void testGetSize() {
+        // Initialize dictionary with one entry.
+        LzwDictionary subject = new LzwDictionary();
+        subject.addData("ff");
+
+        // Dictionary should be of size 1.
+        assertEquals(1, subject.getSize());
+    }
+
+    public void testIsFull() {
+        // Initialize dictionary with one entry.
+        LzwDictionary subject = new LzwDictionary();
+        subject.addData("ff");
+
+        // Dictionary should not be full at this stage.
+        assertFalse(subject.isFull());
+
+        // Fill the rest of the dictionary.
+        for (int i = 0; i < Short.MAX_VALUE - 1; i++) {
+            subject.addData(String.valueOf(i));
+        }
+
+        // Dictionary should now be full.
+        assertTrue(subject.isFull());
+    }
 }
